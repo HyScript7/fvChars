5231c42a18d1a93d0ca5e388d5e1729b09be4c7d
HyScript7 <hyscript7@seznam.cz>
2024-04-07 17:04:08 +0200

    Fix: Bad router name when importing v1 API

--- backend/app/routes/v1/__init__.py
+++ backend/app/routes/v1/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
-api: APIRouter = APIRouter()
+router: APIRouter = APIRouter()
 
 from .user_router import router as user_router
 
-api.include_router(user_router, prefix="/user")
+router.include_router(user_router, prefix="/user")
