09c4dc8943bb8e5b9c10e10385a8a015cb49c7ab
HyScript7 <hyscript7@seznam.cz>
2024-04-20 00:10:04 +0200

    🔨 Drop DB when starting tests

--- backend/tests/__init__.py
+++ backend/tests/__init__.py
@@ -1,5 +1,9 @@
+from app import app
+from app.database import Base, engine
+from app.models import *
 from fastapi.testclient import TestClient
 
-from app import app
+Base.metadata.drop_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 client = TestClient(app)
