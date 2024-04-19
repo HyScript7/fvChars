3e89bbdd8246dd89d61d82544635fc35b0696d11
HyScript7 <hyscript7@seznam.cz>
2024-04-19 22:58:57 +0200

    🗃️ Make Characters back populate Users

--- backend/app/models/user_model.py
+++ backend/app/models/user_model.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship, Mapped
+from typing import List
 
 from ..database import Base
 
@@ -8,6 +10,7 @@ class UserModel(Base):
     email = Column(String, unique=True)
     username = Column(String, unique=True, index=True)
     password = Column(String)
+    characters = relationship("CharacterModel", backref="user")
     def __init__(self, email, username, password):
         self.email = email
         self.username = username
