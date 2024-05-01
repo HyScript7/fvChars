26a10194a16c57ee3c6812261ff74f054d96e11b
HyScript7 <hyscript7@seznam.cz>
2024-05-01 16:14:23 +0200

    🥅 Make all user service errors implement a common exception

--- backend/app/services/errors/user_errors.py
+++ backend/app/services/errors/user_errors.py
@@ -1,13 +1,21 @@
-class UsernameAlreadyExistsError(Exception):
+class UserServiceError(Exception):
+    """
+    An exception that can occour while using the User Service.
+    Usage: raise UserServiceException()
+    """
+    ...
+
+
+class UsernameAlreadyExistsError(UserServiceError):
     def __init__(self, username: str) -> None:
         super().__init__(f"The username '{username} is already taken!")
 
 
-class EmailAlreadyExistsError(Exception):
+class EmailAlreadyExistsError(UserServiceError):
     def __init__(self, email: str) -> None:
         super().__init__(f"The email '{email} is already  in use!")
 
 
-class InvalidCredentialsError(Exception):
+class InvalidCredentialsError(UserServiceError):
     def __init__(self) -> None:
         super().__init__("Incorrect username or password!")
