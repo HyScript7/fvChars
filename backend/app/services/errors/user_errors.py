254bcde8b3f16d42a3c40e55eccdc10a2caa0bb6
HyScript7 <hyscript7@seznam.cz>
2024-05-01 17:42:21 +0200

    🔒 Implement JWT in user_service

--- backend/app/services/errors/user_errors.py
+++ backend/app/services/errors/user_errors.py
@@ -1,8 +1,12 @@
+from typing import Optional
+
+
 class UserServiceError(Exception):
     """
     An exception that can occour while using the User Service.
     Usage: raise UserServiceException()
     """
+
     ...
 
 
@@ -17,12 +21,37 @@ class EmailAlreadyExistsError(UserServiceError):
 
 
 class InvalidCredentialsError(UserServiceError):
-    def __init__(self) -> None:
-        super().__init__("Incorrect username or password!")
+    def __init__(self, msg: Optional[str] = None) -> None:
+        super().__init__(msg or "Incorrect username or password!")
+
 
 class UserDoesntExistError(UserServiceError):
     """An exception that can occour when attempting to delete or update a user.
     Usage: raise UserDoesntExistError("foobar")
     """
-    def __init__(self, username: str) -> None:
-        super().__init__(f"User '{username}' does not exist!")
+
+    def __init__(self, id: str) -> None:
+        super().__init__(f"User '{id}' does not exist!")
+
+
+class InvalidJWTError(UserServiceError):
+    """
+    An exception that can occour while using the User Service to decode a JWT.
+    Usage: raise InvalidJWTError()
+    """
+
+    ...
+
+
+class ExpiredJWTError(UserServiceError):
+    def __init__(self) -> None:
+        super().__init__(
+            "The provided JWT token has expired! Please login again and use the new token."
+        )
+
+
+class OutdatedJWTError(UserServiceError):
+    def __init__(self, token_ver: str, server_ver: str) -> None:
+        super().__init__(
+            f"The provided JWT token appears to be corrupted. Is an instance outdated? Your token version is {token_ver}, and the server version is {server_ver}!"
+        )
--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -1,28 +1,141 @@
+from datetime import datetime, timedelta
+from typing import Annotated, Dict
+
+from fastapi import Header, HTTPException, status
+from jose import ExpiredSignatureError, JWTError, jwt
 from passlib.context import CryptContext
 
-
+from ..config import APP_VERSION, JWT_SECRET
 from ..database.models.user_model import (
+    DisplaynameUpdate,
+    PasswordUpdate,
     User,
     UserSignin,
     UserSignup,
-    DisplaynameUpdate,
-    PasswordUpdate,
 )
 from .errors.user_errors import (
     EmailAlreadyExistsError,
+    ExpiredJWTError,
     InvalidCredentialsError,
-    UsernameAlreadyExistsError,
+    OutdatedJWTError,
     UserDoesntExistError,
+    UsernameAlreadyExistsError,
+    UserServiceError,
 )
 
 crypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-"""
-TODO: Replace the entire get_user_by_username method with get_current_user(),
-which will be used using FastAPI's Depends() and
-will return the current user from Session, or None if guest.
-Raises UserDoesntExistError if user doesn't exist in the database.
-"""
+jwt_expires_in: timedelta = timedelta(hours=3)
+
+
+async def required_get_current_user(jwt_token: Annotated[str, Header()]) -> User:
+    """
+    Retrieves the current user based on the provided JWT token.
+    Raises a HTTPException if the user is not authenticated.
+
+    Args:
+        jwt_token (str | None, optional): The JWT token used to authenticate the user. Defaults to None.
+
+    Returns:
+        User
+    """
+    try:
+        user = await get_current_user(jwt_token)
+    except UserServiceError as e:
+        raise HTTPException(
+            status.HTTP_400_BAD_REQUEST,
+            detail=str(e),
+        )
+    if user is None:
+        raise HTTPException(
+            status.HTTP_401_UNAUTHORIZED,
+            detail="You must be authenticated to use this route!",
+        )
+    return user
+
+
+async def get_current_user(
+    jwt_token: Annotated[str | None, Header()] = None
+) -> User | None:
+    """
+    Retrieves the current user based on the provided JWT token.
+
+    Args:
+        jwt_token (str | None, optional): The JWT token used to authenticate the user. Defaults to None.
+
+    Returns:
+        User | None: The current user if the JWT token is valid and the user exists in the database. Returns None if guest.
+
+    Raises:
+        HTTPException: If the JWT token is invalid.
+    """
+    try:
+        return await get_user_from_jwt(jwt_token)
+    except UserServiceError as e:
+        raise HTTPException(
+            status.HTTP_400_BAD_REQUEST,
+            detail=str(e),
+        )
+
+
+async def get_user_from_jwt(
+    jwt_token: Annotated[str | None, Header()] = None
+) -> User | None:
+    """
+    Retrieves the current user based on the provided JWT token.
+
+    Args:
+        jwt_token (str | None): The JWT token used to authenticate the user. Defaults to None if guest.
+
+    Returns:
+        User | None: The current user if the JWT token is valid and the user exists in the database. Returns None if guest.
+
+    Raises:
+        InvalidCredentialsError: If the JWT token is invalid.
+        InvalidCredentialsError: If the JWT token is improperly formatted or the version in the payload does not match the current version.
+        UserDoesntExistError: If the user with the userid specified in the payload does not exist in the database.
+        ExpiredJWTError: If the JWT token has expired.
+    """
+    if jwt_token is None:
+        return None
+    try:
+        payload: Dict[str | None] = jwt.decode(
+            jwt_token, JWT_SECRET, algorithms=["HS256"]
+        )
+    except ExpiredSignatureError:
+        raise ExpiredJWTError()
+    except JWTError:
+        raise OutdatedJWTError("None", APP_VERSION)
+    if payload.get("username") is None:
+        raise OutdatedJWTError(payload.get("v", "Unknown"), APP_VERSION)
+    user: User | None = await User.find_one(User.username == payload["username"])
+    if user is None:
+        raise UserDoesntExistError(payload["username"] or "None")
+    return user
+
+
+async def create_jwt(user: User) -> str:
+    """
+    Encode a **new** JSON Web Token (JWT) for the user with the provided userid and version.
+
+    Raises:
+        UserDoesntExistError: If the user doesn't have an ID, but model instance exists.
+
+    Returns:
+        str: The encoded JWT token.
+    """
+    if user.id is None:
+        raise UserDoesntExistError(user.username)
+    token: str = jwt.encode(
+        {
+            "username": user.username,
+            "v": APP_VERSION,
+            "exp": datetime.now() + jwt_expires_in,
+        },
+        JWT_SECRET,
+        algorithm="HS256",
+    )
+    return token
 
 
 async def get_user_by_username(username: str) -> User:
@@ -136,7 +249,7 @@ async def delete(user: User, user_password: str) -> None:
 
     Args:
         user (User): The user to delete.
-        user_update (UserUpdate): The update data containing the current password.
+        user_password (str): The current (unhashed) password.
 
     Raises:
         InvalidCredentialsError: If the given current password is invalid.
