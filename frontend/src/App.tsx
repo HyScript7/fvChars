53322d57f62e0583e3a59e16ed35a15192a7bbbb
HyScript7 <hyscript7@seznam.cz>
2024-05-09 21:21:07 +0200

    ✨ Link authentication from the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import type { Component } from "solid-js";
 import { Router, Route } from "@solidjs/router";
 import Home from "./pages/Home";
+import AuthForm from "./components/AuthForm";
 
 const App: Component = () => {
   return (
     <>
+      <AuthForm />
       <Router>
         <Route path="/" component={Home} />
       </Router>
--- frontend/src/components/AuthForm.tsx
+++ frontend/src/components/AuthForm.tsx
@@ -1,12 +1,19 @@
 import { Component, createSignal, Setter } from "solid-js";
+import { sessionStore } from "../stores/AuthStore";
+import { signInHandler, signUpHandler } from "../events/AuthHandler";
 
-export const AuthForm: Component =  () => {
+export const AuthForm: Component = () => {
   const [showSignUp, setShowSignUp] = createSignal<number>(0);
 
   const handleToggle = () => setShowSignUp((prev) => (prev + 1) % 2);
 
   return (
-    <div class="absolute z-10 bg-base-100 bg-opacity-50">
+    <div
+      class={
+        "absolute z-10 bg-base-100 bg-opacity-50 backdrop-blur-sm" +
+        (sessionStore.isAuthenticated ? " hidden" : "")
+      }
+    >
       <div class="min-h-[100vh] flex flex-col justify-center min-w-[100vw]">
         <div class="mx-auto card w-96 bg-base-200 shadow-xl min-h-[40vh]">
           {showSignUp() === 1 ? (
@@ -33,12 +40,13 @@ export const SignInForm: Component<FormProps> = ({ setter, toggle }) => {
         <p class="text-xl text-cyan-500">Sign In</p>
       </div>
       <hr class="border-cyan-800" />
-      <form action="?">
+      <form action={signInHandler} method="post">
         <div class="flex flex-col gap-2">
           <label class="input input-bordered flex items-center gap-2">
             <input
               type="text"
               class="grow max-w-full"
+              name="username"
               placeholder="Username"
               value="username"
             />
@@ -47,13 +55,19 @@ export const SignInForm: Component<FormProps> = ({ setter, toggle }) => {
             <input
               type="password"
               class="grow max-w-full"
+              name="password"
               placeholder="Password"
               value="Password"
             />
           </label>
+          <div class="form-control">
+            <label class="label cursor-pointer">
+              <input type="checkbox" checked name="remember" class="checkbox" />
+              <span class="label-text">Remember me</span>
+            </label>
+          </div>
           <button
-            onClick={() => alert("Login button clicked")}
-            type="button"
+            type="submit"
             class="btn bg-cyan-500 text-black hover:bg-cyan-700"
           >
             Login
@@ -88,12 +102,13 @@ export const SignUpForm: Component<SignUpFormProps> = ({ setter, toggle }) => {
         <p class="text-xl text-cyan-500">Sign Up</p>
       </div>
       <hr class="border-cyan-800" />
-      <form action="?">
+      <form action={signUpHandler} method="post">
         <div class="flex flex-col gap-2">
           <label class="input input-bordered flex items-center gap-2">
             <input
               type="email"
               class="grow max-w-full"
+              name="email"
               placeholder="Email"
               value="example@example.com"
             />
@@ -102,6 +117,7 @@ export const SignUpForm: Component<SignUpFormProps> = ({ setter, toggle }) => {
             <input
               type="text"
               class="grow max-w-full"
+              name="username"
               placeholder="Username"
               value="username"
             />
@@ -110,6 +126,7 @@ export const SignUpForm: Component<SignUpFormProps> = ({ setter, toggle }) => {
             <input
               type="password"
               class="grow max-w-full"
+              name="password"
               placeholder="Password"
               value="Password"
             />
@@ -118,13 +135,19 @@ export const SignUpForm: Component<SignUpFormProps> = ({ setter, toggle }) => {
             <input
               type="password"
               class="grow max-w-full"
+              name="password-repeat"
               placeholder="Repeat Password"
               value="Password"
             />
           </label>
+          <div class="form-control">
+            <label class="label cursor-pointer">
+              <input type="checkbox" checked name="remember" class="checkbox" />
+              <span class="label-text">Remember me</span>
+            </label>
+          </div>
           <button
-            onClick={() => alert("Register button clicked")}
-            type="button"
+            type="submit"
             class="btn bg-cyan-500 text-black hover:bg-cyan-700"
           >
             Register
