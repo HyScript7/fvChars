66e03486fc40912e93f5bda39181ad4824215ab9
HyScript7 <hyscript7@seznam.cz>
2024-05-09 20:07:39 +0200

    🧑‍💻 Make Vite proxy API requests to backend dev server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,8 +13,15 @@ export default defineConfig({
   ],
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+    },
   },
   build: {
     target: 'esnext',
   },
 });
+
