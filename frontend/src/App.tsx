362efc1039069a09bf0cf966640e36d94b96364c
HyScript7 <hyscript7@seznam.cz>
2024-05-09 17:56:52 +0200

    🎨 Move Router declaration to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import type { Component } from "solid-js";
-import { Route } from "@solidjs/router";
+import { Router, Route } from "@solidjs/router";
 import Home from "./pages/Home";
 
 const App: Component = () => {
   return (
     <>
-      <Route path="/" component={Home} />
+      <Router>
+        <Route path="/" component={Home} />
+      </Router>
     </>
   );
 };
